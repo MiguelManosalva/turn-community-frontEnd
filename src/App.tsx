249d3f02222e56e37104714371168c98be39efcf
Miguel A. Manosalva Iraira <miguel@hware.cl>
2024-01-06 21:18:09 -0300

    routes updated

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,12 @@ import {
   Routes,
 } from "react-router-dom";
 // import Home from "./pages/Home/Home";
-import Login from "./pages/Session/Login/Login";
-// import Register from "./pages/Register/Register";
-// import Dashboard from "./pages/Dashboard/Dashboard";
-// import AdminDashboard from "./pages/Dashboard/Admin/AdminDashboard";
-// import UserDashboard from "./pages/Dashboard/User/UserDashboard";
-// import ShiftManagement from "./pages/ShiftManagement/ShiftManagement";
-// import UserProfile from "./pages/UserProfile/UserProfile";
-// import Notifications from "./pages/Notifications/Notifications";
-// import Settings from "./pages/Settings/Settings";
 import "./assets/styles/global.scss";
 import "./assets/styles/theme/main.css";
 import "./assets/styles/theme/responsive.css";
 import Main from "./components/layout/Main/Main";
 import Dashboard from "./pages/Dashboard/Dashboard";
+import Login from "./pages/Session/Login/Login";
 import Register from "./pages/Session/Register/Register";
 
 function App() {
